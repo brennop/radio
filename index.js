e5587093bd36e26a4eb661aa79ac63af86078626
brennop <brennopcordeiro@gmail.com>
2021-08-31 15:10:05 -0300

    working

---
+++ index.js
@@ -0,0 +1,56 @@
+require("dotenv").config();
+
+const ytdl = require("ytdl-core");
+const Discord = require("discord.js");
+
+const settings = {
+  prefix: process.env.PREFIX,
+  token: process.env.TOKEN,
+};
+
+const radios = {
+  indie: "https://www.youtube.com/watch?v=oVi5gtzTDx0",
+  games: "https://www.youtube.com/watch?v=Sdf5-3yddvA",
+  lofi1: "https://www.youtube.com/watch?v=5qap5aO4i9A",
+  lofi2: "https://www.youtube.com/watch?v=mu8j6-k4HIM",
+};
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I'm ready !");
+});
+
+client.on("message", async (message) => {
+  if (!message.content.startsWith(settings.prefix)) return;
+
+  const [cmd, arg] = message.content.slice(settings.prefix.length).split(" ");
+
+  if (cmd === "play" || cmd === "p") {
+    if (message.member.voice.channel == null) {
+      const error = "You need to be in a voice channel to use this command.";
+      message.channel.send(error);
+      return;
+    }
+
+    const url = radios[arg];
+    if (url == null) {
+      const error = `Radio not found: ${arg}\nAvailable radios: ${Object.keys(radios).join(", ")}`;
+      message.channel.send(error);
+      return;
+    }
+
+    const stream = ytdl(url);
+
+    message.member.voice.channel
+      .join()
+      .then((connection) =>
+        connection
+          .play(stream, { type: "unknown" })
+          .on("start", () => message.channel.send(`Now playing ${url}`))
+          .on("finish", () => message.guild.me.voice.channel.leave())
+      );
+  }
+});
+
+client.login(settings.token);
